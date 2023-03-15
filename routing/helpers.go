2b3c61fb82300dd7fe37cf116b9741508c956645
Aubree <aubree@blueskylabs.com.au>
2023-03-15 16:09:55 +0800

    Add descriptions to all functions and structs

--- routing/helpers.go
+++ routing/helpers.go
@@ -2,20 +2,28 @@ package routing
 
 import "github.com/gin-gonic/gin"
 
+// Middleware is a function that runs prior to the main function defined for an endpoint.
+// Use the provided *gin.Context to run all required checks.
+// Return true to run the next function.
+// If false is returned, response must be set within the middleware.
 type Middleware = func(c *gin.Context) bool
 
+// ApiOptions defines options to use for a certain endpoint group.
 type ApiOptions struct {
 	Middleware []Middleware
 }
 
+// BaseApi is the base struct for all goApiRouting functions.
 type BaseApi struct {
 	route   string
 	engine  *gin.Engine
 	options ApiOptions
 }
 
+// NoMiddleware returns an empty ApiOptions struct.
 func (_ *BaseApi) NoMiddleware(_ *gin.Context) ApiOptions { return ApiOptions{} }
 
+// runMiddleware runs all middlewares defined within the ApiOptions instance within the targeted BaseApi.
 func (api *BaseApi) runMiddleware(c *gin.Context) bool {
 	if api.options.Middleware != nil {
 		for _, m := range api.options.Middleware {
@@ -28,12 +36,14 @@ func (api *BaseApi) runMiddleware(c *gin.Context) bool {
 	return true
 }
 
+// mergeOptions creates a new ApiOptions instance with all middlewares within both the targeted and supplied ApiOptions instances.
 func (options *ApiOptions) mergeOptions(newOptions ApiOptions) ApiOptions {
 	options.Middleware = append(options.Middleware, newOptions.Middleware...)
 
 	return *options
 }
 
+// WithMiddleware creates a new ApiOptions instance with all provided middlewares.
 func WithMiddleware(middleware ...Middleware) ApiOptions {
 	return ApiOptions{
 		Middleware: middleware,
--- routing/routing.go
+++ routing/routing.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+// InitialiseRoutes initialises all routes defined within the provided functions.
+// Requires a *gin.Engine to be passed. Refer to gin documentation for basic setup.
 func InitialiseRoutes(e *gin.Engine, apiFuncs ...func(api BaseApi)) {
 	api := BaseApi{
 		engine:  e,
@@ -17,6 +19,7 @@ func InitialiseRoutes(e *gin.Engine, apiFuncs ...func(api BaseApi)) {
 	}
 }
 
+// Handle adds a new api endpoint with the provided method, route, and handler.
 func (api *BaseApi) Handle(method string, route string, handler func(c *gin.Context)) {
 	path := api.route + "/" + route
 	var handlerWithMiddleware = func(c *gin.Context) {
@@ -27,22 +30,32 @@ func (api *BaseApi) Handle(method string, route string, handler func(c *gin.Cont
 	api.engine.Handle(method, path, handlerWithMiddleware)
 }
 
+// Get is a shorthand functions for Handle. It has the same functionality as if the user ran `api.Handle(http.MethodGet, ...)`.
+// Get adds a new GET endpoint with the provided route and handler.
 func (api *BaseApi) Get(route string, handler func(c *gin.Context)) {
 	api.Handle(http.MethodGet, route, handler)
 }
 
+// Post is a shorthand functions for Handle. It has the same functionality as if the user ran `api.Handle(http.MethodPost, ...)`.
+// Post adds a new GET endpoint with the provided route and handler.
 func (api *BaseApi) Post(route string, handler func(c *gin.Context)) {
 	api.Handle(http.MethodPost, route, handler)
 }
 
+// Patch is a shorthand functions for Handle. It has the same functionality as if the user ran `api.Handle(http.MethodPatch, ...)`.
+// Patch adds a new GET endpoint with the provided route and handler.
 func (api *BaseApi) Patch(route string, handler func(c *gin.Context)) {
 	api.Handle(http.MethodPatch, route, handler)
 }
 
+// Delete is a shorthand functions for Handle. It has the same functionality as if the user ran `api.Handle(http.MethodDelete, ...)`.
+// Delete adds a new GET endpoint with the provided route and handler.
 func (api *BaseApi) Delete(route string, handler func(c *gin.Context)) {
 	api.Handle(http.MethodDelete, route, handler)
 }
 
+// Group defines a new route group with the provided route, and options.
+// Use the function within the define sub routes for the Group.
 func (api *BaseApi) Group(route string, options ApiOptions, group func(api BaseApi)) {
 	if route != "" {
 		group(BaseApi{
